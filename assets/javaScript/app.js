618b60b235b20a25c583d551b549b5281adac218
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-13 16:35:01 -0500

    social BTNs active
    social btns now redirect to appropiate sites

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
 	// create an array for your projects URLS so you don't have to harcode them into HTML
 	let externalLinks = {
 		project_1: "https://mario-cyber.github.io/Train-Scheduler/",
@@ -8,13 +9,25 @@ $(document).ready(function () {
 		project_3: " https://mario-cyber.github.io/Psychic-Game/",
 
 		project_4: "https://www.google.com/",
+
+		github: "https://github.com/Mario-cyber",
+
+		linkedin: "https://www.linkedin.com/in/mario-rojas/",
+
 	}
 	// cretae an onCLick function that obtains the id of the btn clicked
 	// create a working urls by matching the id obtained with the item in your projects object with the same key
 	// use the location.assing mwthod with your new URL to redirect to your project 
+
 	$(".thumbnail").on("click", function () {
 		let id = $(this).attr("id")
 		url = externalLinks[id]
+		location.assign(url)
+	});
+
+	$(".social").on("click", function () {
+		let alt = $(this).attr("alt")
+		url = externalLinks[alt]
 		location.assign(url)
 	});
 
