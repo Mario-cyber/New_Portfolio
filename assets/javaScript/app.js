c1c36d2a990d8b340b74f2adb92ce187752304f8
Mario_95 <mrojasc95.mr@gmail.com>
2019-11-13 15:46:44 -0500

    OnClick added to Projects
    on click funtionality added to projects
    after clicking, user is redirected to a new page based on the project

---
+++ assets/javaScript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	// create an array for your projects URLS so you don't have to harcode them into HTML
+	let externalLinks = {
+		project_1: "https://mario-cyber.github.io/Train-Scheduler/",
+
+		project_2: " https://mario-cyber.github.io/GifTastic-/",
+
+		project_3: " https://mario-cyber.github.io/Psychic-Game/",
+
+		project_4: "https://www.google.com/",
+	}
+	// cretae an onCLick function that obtains the id of the btn clicked
+	// create a working urls by matching the id obtained with the item in your projects object with the same key
+	// use the location.assing mwthod with your new URL to redirect to your project 
+	$(".thumbnail").on("click", function () {
+		let id = $(this).attr("id")
+		url = externalLinks[id]
+		location.assign(url)
+	});
+
+
+
+})
